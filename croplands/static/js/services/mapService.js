d983de4875ef44cb313ab38371cc999b79a8d329
croplands-dev <croplands.dev@gmail.com>
2017-04-20 17:39:07 -0700

    Updated South and North America

--- croplands/static/js/services/mapService.js
+++ croplands/static/js/services/mapService.js
@@ -148,7 +148,7 @@ app.factory('mapService', ['leafletLayers', function (leafletLayers) {
                             subdomains: 'abc',
                             //bounds: L.latLngBounds(L.latLng(69.330539, -141.346028), L.latLng(6.112637, -53.773761))
                         },
-                        url: '//{s}.tiles.croplands.org/{z}/{x}/{y}/tile.png?collection=users/croplandsdev/products&id=NorthAmerica_30m_L1_v20170415&band={band}'
+                        url: '//{s}.tiles.croplands.org/{z}/{x}/{y}/tile.png?collection=users/croplandsdev/products&id=NorthAmerica_30m_L1_v20170419&band={band}'
                     },
                     legend: [
                         {label: 'Croplands', color: '#00FF00'}
@@ -186,7 +186,7 @@ app.factory('mapService', ['leafletLayers', function (leafletLayers) {
                             subdomains: 'abc',
                             //bounds: L.latLngBounds(L.latLng(12.835778465638036, -81.95811941094321), L.latLng(-56.073447989999984, -31.449983235209473))
                         },
-                        url: '//{s}.tiles.croplands.org/{z}/{x}/{y}/tile.png?collection=users/croplandsdev/products&id=SouthAmerica_30m_L1_v20170415&band={band}'
+                        url: '//{s}.tiles.croplands.org/{z}/{x}/{y}/tile.png?collection=users/croplandsdev/products&id=SouthAmerica_30m_L1_v20170419&band={band}'
                     },
                     legend: [
                         {label: 'Cropland', color: '#00FF00'}
